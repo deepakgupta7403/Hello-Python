a158f72249c4446607fe0ad7ba4e0d731bfb3579
Deepak Gupta <deepakgupta7403@gmail.com>
2020-02-18 10:02:00 +0530

    first commit Basic Learn DG Commit

---
+++ Python Basic/GetStartedWithPython.py
@@ -0,0 +1,78 @@
+add = 5 + 4
+print(add)
+
+sub = 5 - 4
+print(sub)
+
+# Float Division
+# It'll Return Float Value
+div = 10 / 2
+print(div)
+
+# Integer Division
+# It'll Return Int Value
+div = 10 // 2
+print(div)
+
+multi = 10 * 2
+print(multi)
+
+bracketE = 8 + 2 * 3  # First It'll Multiply then Add The standard Rule
+print(bracketE)
+
+# Here We Follow "BODMAS" Rule
+# BODMAS RULE SAY
+# B = BRACKETS {},(),[]
+# O = ORDERS (X raised to 2 and Cube Value)
+# D = DIVIDE
+# M = MULTIPLY
+# A = ADD
+# S = SUBTRACT
+bodmasRule = (8 + 2) * 3  # First It'llAdd then Multiply
+print(bodmasRule)
+
+# It means 2 Raised to 3 or 2 PowerOF 3  (Exponent)
+powerOf = 2 * 2 * 2
+print(powerOf)
+
+# If 2 Raised to 10 or 2 PowerOF 10
+powerOf = 2 ** 10
+print(powerOf)
+
+# Find remainder from Div for this We use Modules Sign(%)
+remainder = 10 % 3
+print(remainder)
+
+print('Deepak with single quote')
+
+print("Deepak with double quote")
+
+# What if Its Deepak's The we use Double Quote
+print("Deepak's with double quote")
+
+# What if Its "Deepak" The we use Quote
+print('"Deepak" with single double quote')
+
+# What if we use single and Double Quote Both
+# Then We escalate the Single quote With \ Sign
+print("Deepak with 'Single'  \"double\" quote")
+
+# Concate Of 2 String
+print('Deepak' + ' gupta')
+
+# Concate Of 2 String
+print('Deepak' + ' gupta')
+
+# Concate Of 10 String
+print(10 * 'Deepak')
+
+# In Python \n say the Python to print to new line
+print('c:\docs\not')
+
+# If we want to print the whole word without any special charecter or convert useing any Special charecter
+# Then We use r that tells the Python to its RAW Word
+print(R'c:\docs\not')
+print(r'c:\docs\not')
+
+
+
